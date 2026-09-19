2b307dc650d550e7f1c3391e684427e39aa49a35
YNSStudios <yurinascimentosouza5@gmail.com>
2026-09-18 23:12:09 -0300

    Finalização da função 1 as 22:20 e termino da função 2 as 23:09

--- app.py
+++ app.py
@@ -1,11 +1,180 @@
 import json
-import random 
+import random
 from time import sleep
 
 try:
-    with open('produtos.josn', 'r', encoding='utf-8') as dados:
+    with open("produtos.json", "r", encoding="utf-8") as dados:
         produtos = json.load(dados)
 
 except FileNotFoundError:
     produtos = []
 
+sleep(0.6)
+
+print("=================")
+print("[1] Criar produtos")
+print()
+print("[2] Editar produtos")
+print()
+
+while True:
+    escolha = input("Digite aqui: ")
+    print()
+
+    if escolha.isnumeric():
+        escolha = int(escolha)
+        break
+
+    else:
+        print("\033[31mIsso não é um número !\033[0m")
+        print()
+
+while True:
+
+    if escolha == 1:
+
+        print("\033[33mCriação de produtos !\033[0m")
+        print()
+
+        while True:
+
+            qtd_produtos = input("Digite quantos produtos você vai cadastrar: ")
+            print()
+            if qtd_produtos.isnumeric():
+                qtd_produtos = int(qtd_produtos)
+                break
+            else:
+                print("Isso não é um número !")
+                print()
+
+        for c in range(qtd_produtos):
+
+            while True:
+
+                codigo = random.randint(1000, 9999)
+
+                codigo_encontrado = None
+
+                for produto in produtos:
+                    if produto["codigo"] == codigo:
+                        codigo_encontrado = True
+                        break
+                    elif produtos is None:
+                        break
+
+                if codigo_encontrado == None:
+                    break
+
+            nome = input("Digite o nome do produto: ").title().strip()
+            print()
+            print("Apenas números")
+            print()
+            preço = float(input("Digite o preço do produto: ").replace(",", "."))
+            print()
+            estoque = int(input("Digite o estoque do produto: "))
+            print()
+            classe = (
+                input("o que é seu produto ? eletrônico, domestico ... : ")
+                .capitalize()
+                .strip()
+            )
+            print()
+
+            produtos.append(
+                {
+                    "nome": nome,
+                    "preço": preço,
+                    "estoque": estoque,
+                    "classe": classe,
+                    "codigo": codigo,
+                }
+            )
+
+            print("\033[32mProduto criado !\033[0m")
+            print()
+
+        with open("produtos.json", "w", encoding="utf-8") as dados:
+            json.dump(produtos, dados, ensure_ascii=False, indent=5)
+
+        break
+
+    elif escolha == 2:
+
+        print("\033[33mEdição de produtos !\033[0m")
+        print()
+
+        for produto in produtos:
+            sleep(0.5)
+            print(f"nome: {produto['nome']}")
+            print(f"Código: {produto['codigo']}")
+            print()
+            sleep(0.5)
+
+        while True:
+
+            while True:
+
+                codigo_busca = input("Digite o codigo do produto: ")
+                print()
+                if codigo_busca.isnumeric():
+                    codigo_busca = int(codigo_busca)
+                    break
+
+            produto_encontrado = None
+
+            for produto in produtos:
+                if produto["codigo"] == codigo_busca:
+                    produto_encontrado = produto
+                    break
+
+            if produto_encontrado is not None:
+                print("\033[32mProduto encontrado !\033[0m")
+                print()
+                break
+
+        print(f"Nome: {produto_encontrado['nome']}")
+        print(f"Estoque: {produto_encontrado['estoque']}")
+        print(f"Preço: R${produto_encontrado['preço']}")
+        print()
+
+        print("O que você vai editar: Nome, Estoque, Preço ? ")
+        print()
+
+        while True:
+
+            ediçao = input("Digite aqui: ").lower()
+            print()
+
+            if ediçao == "nome":
+
+                novo_nome = input("Digite o novo nome: ").title().strip()
+                produto_encontrado["nome"] = novo_nome
+                break
+
+            elif ediçao == "estoque":
+
+                novo_estoque = int(input("Digite o novo estoque"))
+                produto_encontrado["estoque"] = novo_estoque
+                break
+
+            elif ediçao == "preço":
+
+                novo_preço = float(input("Digite o novo preço: ").replace(',', '.'))
+                produto_encontrado["preço"] = novo_preço
+                break
+
+            else:
+                print("\033[31mOpção inválida ! tente novamente. \033[0m")
+
+        with open("produtos.json", "w", encoding="utf-8") as dados:
+            json.dump(produtos, dados, ensure_ascii=False, indent=5)
+
+            print()
+            print(f"Nome: {produto_encontrado['nome']}")
+            print(f"Estoque: {produto_encontrado['estoque']}")
+            print(f"Preço: R${produto_encontrado['preço']}")
+            print()
+
+            print("\033[32mProduto editado com sucesso !\033[0m")
+            print()
+            break
